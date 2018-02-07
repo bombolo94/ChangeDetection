42c6fa64b2cd5400c470438fee52495c252e8e08
bombolo94 <bomb-94@hotmail.it>
2018-02-07 17:05:30 +0100

    I'm just kidding

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ import utils as ut
 cap = cv2.VideoCapture('video.avi')
 nFrame = 0
 T = 20
+med = 100
 
 while cap.isOpened():
     ret, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     # this part is background initialisation according to ChangeDetection.pdf
-    if nFrame < 100:
+    if nFrame < med:
         if nFrame == 0:
             prev = gray.copy()
         s = cv2.add(gray.astype(np.int32), prev.astype(np.int32))
@@ -18,28 +19,30 @@ while cap.isOpened():
 
     # this part is background subst and updating according to ChangeDetection.pdf
     else:
-        if nFrame == 100:
-            bckI = (s/100).astype(np.uint8)
-
-        if key==119:
-            T+=1
-        elif key==115:
-            T-=1
-        c_mask = cv2.threshold(cv2.absdiff(gray, bckI), T, 255, cv2.THRESH_BINARY)[1]
+        if nFrame == med:
+            bckI = (s/med).astype(np.uint8)
+        '''c_mask = cv2.threshold(cv2.absdiff(gray, bckI), T, 255, cv2.THRESH_BINARY)[1]
         cv2.imshow("Threshold-Substraction", c_mask)
         cv2.imshow("BckI", bckI)
-        opening = c_mask.copy()
         # Morphology
-        '''kernel = np.ones((3, 3), np.uint8)
-        opening = cv2.dilate(c_mask, kernel, iterations=0)
-        opening = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, cv2.getStructuringElement(cv2.MORPH_RECT, (12, 12)))
-        opening = cv2.morphologyEx(opening, cv2.MORPH_OPEN, cv2.getStructuringElement(cv2.MORPH_RECT, (7, 7)))
+
+        kernel = np.array([[0,1,0],[1,1,1],[0,1,1]])
+        morf = cv2.dilate(c_mask, kernel, iterations=0)
+        kernel = np.array([[0, 0, 0, 0, 0, 0, 0],
+       [0, 0, 1, 1, 1, 0, 0],
+       [0, 0, 1, 1, 1, 0, 0],
+       [0, 0, 1, 1, 1, 0, 0],
+       [0, 0, 1, 1, 1, 0, 0],
+       [0, 0, 1, 1, 1, 0, 0],
+       [0, 0, 0, 0, 0, 0, 0]])
+        morf = cv2.morphologyEx(morf, cv2.MORPH_CLOSE, cv2.getStructuringElement(kernel))
+        morf = cv2.morphologyEx(morf, cv2.MORPH_OPEN, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (7,7)))
         
         denoised = cv2.GaussianBlur(opening, (5, 5), 0)
         filter = cv2.Laplacian(denoised, cv2.CV_64F)
-        cv2.imshow('Laplacian Filter', filter)'''
+        cv2.imshow('Laplacian Filter', filter)
 
-        #cv2.imshow("Morphology", opening)
+        cv2.imshow("Morphology", morf)
 
         # Blob Analysis
 
@@ -54,22 +57,22 @@ while cap.isOpened():
         detector = cv2.SimpleBlobDetector_create(params)
         
         # Detect blobs
-        rv = 255 - opening
+        rv = 255 - morf
         keypoints = detector.detect(rv)
 
         # keypoints is a list of keypoint, which include the coordinates (of the centres) of the blobs, and their size
 
-        im_with_keypoints = cv2.drawKeypoints(opening, keypoints, np.array([]), (0, 0, 155),
+        im_with_keypoints = cv2.drawKeypoints(morf, keypoints, np.array([]), (0, 0, 155),
                                               cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
         cv2.imshow('BLOB', im_with_keypoints)
         # End Blob Analysis
 
         # Backround Updating
-        bckU = ut.updating_background(opening, gray, bckI, 0.1)
-        bckI = bckU.copy()
-        print(nFrame)
+        bckU = ut.updating_background(c_mask, gray, bckI, 0.5)
+        bckI = bckU.copy()'''
     nFrame = nFrame + 1
-    key = cv2.waitKey(1)
+    cv2.imshow('Gray', gray)
+    key = cv2.waitKey(0)
 
 # When everything done, release the capture
 cap.release()
--- utils.py
+++ utils.py
@@ -5,23 +5,11 @@ def updating_background(c_mask, frame, bck, alpha):
     bck_upd = bck.copy()
     for row in range(width):
         for col in range(height):
-            v = c_mask[row, col]
-            if v == 0:
+            if c_mask[row, col] == 0:
                 p = alpha * frame[row, col] + (1 - alpha) * bck[row, col]
             else:
                 p = bck[row, col]
             bck_upd[row, col] = p
     return bck_upd
 
-def trheshold(dif,T):
-    width, height = dif.shape[:2]
-    tr = dif.copy()
-    for row in range(width):
-        for col in range(height):
-            if dif[row, col] >T:
-                po = 255
-            else:
-                po = 0
-            tr[row, col] = po
-    return tr
 
